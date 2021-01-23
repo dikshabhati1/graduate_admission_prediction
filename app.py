1b8d828e1c904cecd6fb53bf44fb8223542c8439
Diksha Bhati <70757239+dikshabhati1@users.noreply.github.com>
2021-01-23 19:24:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+import jsonify
+import requests
+import pickle
+import numpy as np
+import sklearn
+from sklearn.preprocessing import StandardScaler
+app = Flask(__name__)
+model = pickle.load(open('graduate_admission.pkl', 'rb'))
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+
+standard_to = StandardScaler()
+@app.route("/predict", methods=['POST'])
+def predict():
+    
+    if request.method == 'POST':
+        GRE_Score = int(request.form['GRE Score'])
+        TOEFL_Score=int(request.form['TOEFL Score'])
+        University_Rating=int(request.form['University Rating'])
+        SOP=float(request.form['SOP'])
+        LOR=float(request.form['LOR'])
+        CGPA=float(request.form['CGPA'])
+        Research=int(request.form['Research'])
+       
+        prediction=model.predict([[GRE_Score,TOEFL_Score,University_Rating,SOP,LOR,CGPA,Research]])
+        output=round(prediction[0],2)
+        if output<0:
+            return render_template('index.html',prediction_texts="Sorry you cannot take admission")
+        else:
+            return render_template('index.html',prediction_text="Youe Admission probability is {}".format(output))
+    else:
+        return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
